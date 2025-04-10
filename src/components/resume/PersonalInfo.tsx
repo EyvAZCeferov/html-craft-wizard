f9ff123fd1aa95075ffb11de4f81018c454721e7
Eyvaz Cəfərov <45921802+EyvAZCeferov@users.noreply.github.com>
2025-04-10 09:16:38 +0400

    text-centered

--- src/components/resume/PersonalInfo.tsx
+++ src/components/resume/PersonalInfo.tsx
@@ -5,39 +5,39 @@ const PersonalInfo = () => {
   return (
     <div className="bg-gray-700 text-white p-4">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className="flex flex-row">
+        <div className="flex flex-row text-center">
           <span className="font-semibold">Doğum tarixi:</span>
           <span>&nbsp; 01.05.1985</span>
         </div>
-        <div className="flex flex-row">
+        <div className="flex flex-row text-center">
           <span className="font-semibold">Ailə vəziyyəti:</span>
           <span>&nbsp; Subay</span>
         </div>
-        <div className="flex flex-row">
+        <div className="flex flex-row text-center">
           <span className="font-semibold">Hərbi xidmət:</span>
           <span>&nbsp; 2007 - 2008</span>
         </div>
-        <div className="flex flex-row">
+        <div className="flex flex-row text-center">
           <span className="font-semibold">Vətəndaşlıq:</span>
           <span>&nbsp; Azərbaycan</span>
         </div>
-        <div className="flex flex-row">
+        <div className="flex flex-row text-center">
           <span className="font-semibold">Cinsi:</span>
           <span>&nbsp; Kişi</span>
         </div>
-        <div className="flex flex-row">
+        <div className="flex flex-row text-center">
           <span className="font-semibold">Sürücülük vəsiqəsi:</span>
           <span>&nbsp; B kateqoriyası</span>
         </div>
-        <div className="flex flex-row">
+        <div className="flex flex-row text-center">
           <span className="font-semibold">Qeydiyyat ünvanı:</span>
           <span>&nbsp; Bakı şəhəri</span>
         </div>
-        <div className="flex flex-row">
+        <div className="flex flex-row text-center">
           <span className="font-semibold">Əlillik dərəcəsi:</span>
           <span>&nbsp; Yoxdur</span>
         </div>
-        <div className="flex flex-row">
+        <div className="flex flex-row text-center">
           <span className="font-semibold">Məhkumluq statusu:</span>
           <span>&nbsp; Yoxdur</span>
         </div>
