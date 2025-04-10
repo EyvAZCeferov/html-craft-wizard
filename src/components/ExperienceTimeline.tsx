429248dc4f90405aa224b6ac95e64dd02201872f
Eyvaz Cəfərov <45921802+EyvAZCeferov@users.noreply.github.com>
2025-04-10 08:44:12 +0400

    top 25

--- src/components/ExperienceTimeline.tsx
+++ src/components/ExperienceTimeline.tsx
@@ -19,8 +19,8 @@ const ExperienceTimeline: React.FC<ExperienceTimelineProps> = ({ experiences }) 
       {experiences.map((experience, index) => (
         <div key={index} className="relative pl-8 pb-8 border-l border-gray-300">
           {/* Timeline dot */}
-          <div className="absolute left-0 top-0 transform -translate-x-1/2 bg-white p-1">
-            <div className="w-2 h-2 bg-gray-500 rotate-45"></div>
+          <div className="absolute left-0 top-0 transform -translate-x-1/2 bg-transparent p-1 position-relative">
+            <div className="w-2 h-2 bg-gray-500 m-2 rotate-45 position-absolute top-25"></div>
           </div>
 
           {/* Date range */}
