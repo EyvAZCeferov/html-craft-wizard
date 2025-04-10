ebed7c60c34c4ec7e3c2cbf63722fb88e95e4bd2
Eyvaz Cəfərov <45921802+EyvAZCeferov@users.noreply.github.com>
2025-04-10 08:28:04 +0400

    header bg changed

--- src/components/resume/Header.tsx
+++ src/components/resume/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
     <div className="relative">
       <div className="flex flex-col md:flex-row">
         {/* Left Header - Name and Title */}
-        <div className="bg-white p-6 md:p-10 md:w-2/5 relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-gray-300">
+        <div className="bg-gray-100 p-6 md:p-10 md:w-2/5 relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-gray-300">
           <h1 className="text-3xl font-bold text-gray-800 tracking-tight">
             ORXAN XUDUYEV
           </h1>
@@ -25,7 +25,7 @@ const Header = () => {
         </div>
 
         {/* Right Header - Contact Info with Photo */}
-        <div className="bg-gray-100 p-6 md:p-10 md:w-3/5 relative before:content-[''] before:absolute before:top-0 before:right-0 before:w-full before:h-0.5 before:bg-gray-300">
+        <div className="bg-white p-6 md:p-10 md:w-3/5 relative before:content-[''] before:absolute before:top-0 before:right-0 before:w-full before:h-0.5 before:bg-gray-300">
           <div className="flex flex-col md:flex-row items-center">
             <div className="w-40 h-40 rounded-full overflow-hidden border-4 border-red-700 mb-6 md:mb-0 md:mr-6 flex-shrink-0">
               <img 
