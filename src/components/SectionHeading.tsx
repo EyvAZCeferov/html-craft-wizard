2e3ad826456a9a418c3cf986c80c55e0fed56e4a
Eyvaz Cəfərov <45921802+EyvAZCeferov@users.noreply.github.com>
2025-04-10 08:36:30 +0400

    bg transparented

--- src/components/SectionHeading.tsx
+++ src/components/SectionHeading.tsx
@@ -11,7 +11,7 @@ const SectionHeading: React.FC<SectionHeadingProps> = ({ title }) => {
       <h3 className="text-2xl font-bold tracking-wide text-black text-center">{title}</h3>
       <div className="absolute bottom-0 left-0 w-full border-b border-gray-300"></div>
       <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -mb-1">
-        <div className="bg-white px-2">
+        <div className="bg-transparent px-2">
           <div className="h-2 w-8 flex items-center justify-center">
             <div className="w-2 h-2 border border-gray-400 rotate-45 mx-0.5"></div>
             <div className="w-2 h-2 border border-gray-400 rotate-45 mx-0.5"></div>
